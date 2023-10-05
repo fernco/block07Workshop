d789e6d878051277fbfe540ec93f054cff26ac01
Fern Coelho <123507401+fernco@users.noreply.github.com>
2023-10-05 11:26:41 -0700

    finished workshop 7

---
+++ vault.js
@@ -0,0 +1,36 @@
+// combination code : 10 
+// declared variable for two numbers - sum of two numbers = first combo
+let num1 = 9
+let num2 = 1
+let firstCombo = num1 + num2 
+
+console.log(firstCombo); 
+
+
+
+// combination code : 40
+// declared variable to 40. Both statements need to be true or it will be false. 
+// In my example 40 = 40 and 40 % 2 is even number so it is true 
+let secondCombo = 40 
+let isTrue = (secondCombo === 40) && (secondCombo % 2 === 0); 
+
+console.log(isTrue); 
+
+
+// combination code : 30
+// wanted to use multiplication for this combo. multipled two numbers that = 30 
+
+let thirdCombo = 30
+let num3 = 15
+let num4 = 2
+let multiply = (num3 * num4 === thirdCombo); 
+
+console.log(multiply); 
+
+// popup! 
+// created an alert w/ numbers of the vault & then added alert with string stating what was popping up 
+
+let vaultCode = "10, 40, 30" 
+alert("The vault code is: " + vaultCode)
+
+console.log(alert); 
